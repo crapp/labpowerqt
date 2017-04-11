03def6b0433e7a13f75e0c3114e6e152a1dcbc79
Christian Rapp <0x2a@posteo.org>
2017-04-11 11:56:42 +0200

    Fix additional QByteArray to std::string occurence

--- src/powersupplyscpi.cpp
+++ src/powersupplyscpi.cpp
@@ -1,3 +1,19 @@
+// labpowerqt is a Gui application to control programmable lab power supplies
+// Copyright © 2015, 2016, 2017 Christian Rapp <0x2a at posteo dot org>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "powersupplyscpi.h"
 
 namespace powcon = PowerSupplySCPI_constants;
@@ -155,7 +171,9 @@ void PowerSupplySCPI::readWriteData(std::shared_ptr<SerialCommand> com)
                         }
                     } else {
                         log.eal_error("Wait for ready read for command " +
-                                      commandByte.toStdString() + " timed out");
+                                      std::string(commandByte.constData(),
+                                                  commandByte.length()) +
+                                      " timed out");
                         log.eal_error(
                             "Error: " +
                             static_cast<QString>(this->serialPort->error())
